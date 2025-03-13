2c7cad7816d8377084e3eb39514862e78a231d31
Ezequiel Lizarraga <ezeq.lizarraga@gmail.com>
2025-03-12 22:42:13 -0300

    agregando objetos y funciones del orden superior

--- app.js
+++ app.js
@@ -1,67 +1,99 @@
 
-const shop = ["Servilleta", "Papel higienico", "Toalla intercalada", "Detergente"];
-const precio_x_producto = 500;
 
-const mi_carrito = [];
-let mi_dinero = 1500;
+
+class Item {
+    constructor(nombre, precio, tipo, descripcion) {
+        this.nombre = nombre;
+        this.precio = precio;
+        this.tipo = tipo;
+        this.descripcion = descripcion;
+    }
+}
+
+const shop = [
+    new Item("Servilleta", 500, "papel", "Una servilleta."),
+    new Item("Papel Higienico", 1000, "papel", "Un papel higienico."),
+    new Item("Toalla intercalada", 800, "papel", "Un seca manos."),
+    new Item("Detergente", 300, "limpieza", "Un detergente limpiador."),
+];
+
+
+let cliente = {
+    carrito: [],
+    dinero: 15000,
+};
+
+if (localStorage.getItem("cliente")) {
+    cliente = parseInt(localStorage.getItem("cliente"));
+  }
 
 function comprar_prompt() {
-const item_a_comprar = prompt("¿Qué deseas comprar?");
-comprar_item(item_a_comprar);
+    const item_a_comprar = prompt("¿Qué deseas comprar?");
+    comprar_item(item_a_comprar);
 }
 
-function vender_prompt() {
-const item_a_vender = prompt("¿Qué deseas vender?");
-vender_item(item_a_vender);
+function eliminar_prompt() {
+    const item_a_eliminar = prompt("¿Qué deseas Eliminar?");
+    eliminar_item(item_a_eliminar);
 }
 
-
-function comprar_item(item) {
-if (shop.includes(item)) {
-    if (mi_dinero >= precio_x_producto) {
-    mi_dinero = mi_dinero - precio_x_producto;
-    const indice = shop.indexOf(item);
-    mi_carrito.push(shop[indice]);
-    console.log("¡Compraste " + item + "!");
+function comprar_item(prompt) {
+    let item_a_comprar = false;
+    for (item of shop) {
+      if (item.nombre == prompt) {
+        item_a_comprar = item;
+      }
+    }
+  
+    if (item_a_comprar) {
+      if (cliente.dinero >= item_a_comprar.precio) {
+        cliente.dinero = cliente.dinero - item_a_comprar.precio;
+        localStorage.setItem("cliente.dinero", cliente.dinero);
+        cliente.carrito.push(item_a_comprar);
+        localStorage.setItem("cliente.carrito", JSON.stringify(cliente.carrito));
+        console.log("¡Compraste " + item_a_comprar.nombre + "!");
+      } else {
+        console.warn(
+          "No tenés dinero suficiente para comprar " +
+            item_a_comprar.nombre +
+            ". Tu dinero es: " +
+            cliente.dinero +
+            ". Costo del producto: " +
+            item_a_comprar.precio
+        );
+      }
     } else {
-    console.warn(
-        "No tenés dinero suficiente para comprar " +
-        item +
-        ". Tu dinero es: " +
-        mi_dinero +
-        ". El costo del producto es:$ " +
-        precio_x_producto
-    );
+      console.error("No tengo nada de '" + prompt + "' disponible a la venta.");
     }
-} else {
-    console.error("No tengo nada de '" + item + "' disponible a la venta.");
-}
-}
+  }
 
-
-function vender_item(item) {
-if (mi_carrito.includes(item)) {
-    const indice = mi_carrito.indexOf(item);
-    mi_carrito.splice(indice, 1);
-    console.log("¡Vendiste " + item + "!");
-    mi_dinero = mi_dinero + precio_x_producto;
-    mostrar_carrito();
-} else {
-    console.error("No tenés nada de '" + item + "' para vender.");
-}
-}
-
-function mostrar_carrito() {
-console.log("Tu Carrito:");
-for (let indice = 0; indice < mi_carrito.length; indice++) {
-    console.log(mi_carrito[indice]);
-}
-console.log("Tu dinero:$", mi_dinero);
-}
-
-function mostrar_shop() {
-    console.log("Los productos disponibles son:");
-    for (let indice = 0; indice < shop.length; indice++) {
-        console.log(shop[indice]);
+function eliminar_item(prompt) {
+    let item_a_eliminar = false;
+    for (item of cliente.carrito) {
+      if (item.nombre == prompt) {
+        item_a_eliminar = item;
+      }
     }
+    if (item_a_eliminar) {
+      const indice = cliente.carrito.indexOf(item_a_eliminar);
+      cliente.carrito.splice(indice, 1);
+      localStorage.setItem("cliente", JSON.stringify(cliente.carrito));
+      console.log("¡Eliminaste " + item_a_eliminar.nombre + "!");
+      cliente.dinero = cliente.dinero + item_a_eliminar.precio;
+      localStorage.setItem("cliente", cliente.dinero);
+      mostrar_carrito();
+    } else {
+      console.error("No tenés ningun item para eliminar.");
     }
+  }
+  
+  function mostrar_carrito() {
+    console.log("Carrito:");
+    for (item_carrito of cliente.carrito) {
+      console.log("- " + item_carrito.nombre);
+    }
+    console.log("Dinero:", cliente.dinero);
+
+  }
+
+
